6df40b44072050bae990e2402b64b3f50784fecb
islom1712 <127305247+islom1712@users.noreply.github.com>
2023-06-02 15:31:02 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace Tasvirlarni_siqish_algoritmi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.savefilebtn = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.radio2 = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.radio1 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(557, 443);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(566, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(558, 443);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(16, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 52);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(577, 41);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(247, 56);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Value = 50;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(907, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(208, 56);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "compress";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 39);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Filesize";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 39);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Filesize";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(658, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(262, 29);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "30% fayl hajmi kamaydi";
+            this.label3.Visible = false;
+            // 
+            // savefilebtn
+            // 
+            this.savefilebtn.Dock = System.Windows.Forms.DockStyle.Right;
+            this.savefilebtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.savefilebtn.Location = new System.Drawing.Point(983, 0);
+            this.savefilebtn.Name = "savefilebtn";
+            this.savefilebtn.Size = new System.Drawing.Size(144, 100);
+            this.savefilebtn.TabIndex = 6;
+            this.savefilebtn.Text = "Saqlash";
+            this.savefilebtn.UseVisualStyleBackColor = true;
+            this.savefilebtn.Click += new System.EventHandler(this.savefilebtn_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox2, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 100);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1127, 449);
+            this.tableLayoutPanel1.TabIndex = 8;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radio1);
+            this.panel1.Controls.Add(this.radio2);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.trackBar1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1127, 100);
+            this.panel1.TabIndex = 9;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.savefilebtn);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 549);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1127, 100);
+            this.panel2.TabIndex = 9;
+            // 
+            // radio2
+            // 
+            this.radio2.AutoSize = true;
+            this.radio2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radio2.Location = new System.Drawing.Point(134, 50);
+            this.radio2.Name = "radio2";
+            this.radio2.Size = new System.Drawing.Size(365, 35);
+            this.radio2.TabIndex = 5;
+            this.radio2.Text = "Sifatini saqlab qolgan holda";
+            this.radio2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(585, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(222, 29);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "qisqartirish darajasi";
+            // 
+            // radio1
+            // 
+            this.radio1.AutoSize = true;
+            this.radio1.Checked = true;
+            this.radio1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radio1.Location = new System.Drawing.Point(134, 9);
+            this.radio1.Name = "radio1";
+            this.radio1.Size = new System.Drawing.Size(402, 35);
+            this.radio1.TabIndex = 5;
+            this.radio1.TabStop = true;
+            this.radio1.Text = "Sifatini hisobga olmagan holda";
+            this.radio1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1127, 649);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button savefilebtn;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radio1;
+        private System.Windows.Forms.RadioButton radio2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tasvirlarni_siqish_algoritmi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string inputImagePath = null;
+        string outputImagePath = "output.jpg";
+        string decomp = "output2.jpg";
+        float firstsize = 0;
+        float lastsize = 0;
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opf=new OpenFileDialog();
+            opf.Filter = "image files|*.jpg|png file|*.png";
+            if(opf.ShowDialog()==DialogResult.OK )
+            {
+                inputImagePath= opf.FileName;
+                pictureBox1.Image=Image.FromFile(inputImagePath);
+                FileInfo fileInfo = new FileInfo(inputImagePath);
+                label1.Text ="Ochilgan fayl hajmi:"+ Math.Round(fileInfo.Length / 1024.0,2).ToString() + " kb"; ;
+                label1.Visible = true;
+                firstsize= fileInfo.Length;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(inputImagePath!=null)
+            {
+                pictureBox1.Image = null;
+                pictureBox2.Image = null;
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+
+                int compressionLevel = 100 - trackBar1.Value; // Specify the compression level (0-100)
+
+                //  ReduceImageSize2(inputImagePath, outputImagePath);
+                if (radio1.Checked)
+                {
+                    ReduceImageMemorySize(inputImagePath, outputImagePath, compressionLevel);
+                    pictureBox1.Image = Image.FromFile(inputImagePath);
+                    pictureBox2.Image = Image.FromFile(outputImagePath);
+                }
+                else
+                {
+
+                    CompressImage(inputImagePath, outputImagePath);
+                    DecompressImage(outputImagePath, decomp);
+                    pictureBox1.Image = Image.FromFile(inputImagePath);
+                    pictureBox2.Image = Image.FromFile(decomp);
+
+                }
+
+
+                FileInfo fileInfo = new FileInfo(outputImagePath);
+                label2.Visible = true;
+                label2.Text = "Siqilgan Fayl hajmi: " + Math.Round(fileInfo.Length / 1024.0, 2).ToString() + " kb";
+                lastsize = fileInfo.Length;
+                double foiz = Math.Round(100 - (lastsize / firstsize * 100.0), 2);
+                label3.Visible = true;
+                label3.Text = foiz.ToString() + "% Fayl hajmi qisqardi";
+            }
+            
+
+
+
+        }
+        void ReduceImageMemorySize(string inputImagePath, string outputImagePath, int compressionLevel)
+        {
+            using (Image image = Image.FromFile(inputImagePath))
+            {
+                // Create an EncoderParameters object to set the compression level
+                EncoderParameters encoderParams = new EncoderParameters(1);
+                encoderParams.Param[0] = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, compressionLevel);
+
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                // Get the JPEG codec
+                ImageCodecInfo jpegCodec = GetEncoderInfo("image/jpeg");
+
+                // Save the image with the specified compression level
+                image.Save(outputImagePath, jpegCodec, encoderParams);
+               
+
+            }
+        }
+        static void ReduceImageSize2(string inputImagePath, string outputImagePath)
+        {
+            using (Image image = Image.FromFile(inputImagePath))
+            {
+                // Create an EncoderParameters object to specify compression quality
+                EncoderParameters encoderParams = new EncoderParameters(1);
+                encoderParams.Param[0] = new EncoderParameter(System.Drawing.Imaging.Encoder.Compression, (long)EncoderValue.CompressionNone);
+
+                // Get the PNG codec
+                ImageCodecInfo pngCodec = GetEncoderInfo(ImageFormat.Png);
+
+                // Save the image in PNG format
+                image.Save(outputImagePath, pngCodec, encoderParams);
+            }
+        }
+
+        private static ImageCodecInfo GetEncoderInfo(ImageFormat format)
+        {
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.FormatID == format.Guid)
+                {
+                    return codec;
+                }
+            }
+            return null;
+        }
+
+        ImageCodecInfo GetEncoderInfo(string mimeType)
+        {
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageEncoders();
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.MimeType == mimeType)
+                {
+                    return codec;
+                }
+            }
+            return null;
+        }
+
+        private void savefilebtn_Click(object sender, EventArgs e)
+        {
+            if(outputImagePath!= null)
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Jpg file|*.jpg";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    if(radio1.Checked)
+                        File.Copy(outputImagePath, saveFileDialog.FileName);
+                    else
+                        File.Copy(decomp, saveFileDialog.FileName);
+                }
+            }
+            
+        }
+
+        public void CompressImage(string sourceFilePath, string compressedFilePath)
+        {
+            // Read the source image into a byte array
+            byte[] sourceBytes = File.ReadAllBytes(sourceFilePath);
+
+            // Create a compressed stream to write the compressed data
+            using (FileStream compressedFileStream = File.Create(compressedFilePath))
+            {
+                // Create a DeflateStream to perform compression
+                using (DeflateStream deflateStream = new DeflateStream(compressedFileStream, CompressionMode.Compress))
+                {
+                    // Write the source image data to the DeflateStream
+                    deflateStream.Write(sourceBytes, 0, sourceBytes.Length);
+                }
+            }
+             
+        }
+        public static void DecompressImage(string compressedFilePath, string decompressedFilePath)
+        {
+            // Read the compressed data from the file into a byte array
+            byte[] compressedBytes = File.ReadAllBytes(compressedFilePath);
+
+            // Create a decompressed stream to write the decompressed data
+            using (FileStream decompressedFileStream = File.Create(decompressedFilePath))
+            {
+                // Create a DeflateStream to perform decompression
+                using (DeflateStream deflateStream = new DeflateStream(new MemoryStream(compressedBytes), CompressionMode.Decompress))
+                {
+                    // Copy the decompressed data from the DeflateStream to the decompressedFileStream
+                    deflateStream.CopyTo(decompressedFileStream);
+                }
+            }
+
+            Console.WriteLine("Decompression completed successfully.");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            GC.Collect();   
+            GC.WaitForPendingFinalizers();  
+            pictureBox1.Image = Image.FromFile(inputImagePath);
+            pictureBox2.Image = Image.FromFile(decomp);
+        }
+    }
+}
